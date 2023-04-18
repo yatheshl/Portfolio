8d30bef60e52ff364f19fd061ee9cb5bee91f427
Yathesh L <yatheshl@Yatheshs-MacBook-Pro.local>
2023-04-18 15:39:13 -0400

    Changes Made

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+const title = document.getElementById('title');
+let lastScrollTop = 0;
+
+window.addEventListener('scroll', () => {
+  const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (currentScrollTop > lastScrollTop) {
+    title.classList.add('hide-title');
+  } else {
+    title.classList.remove('hide-title');
+  }
+
+  lastScrollTop = currentScrollTop;
+});
